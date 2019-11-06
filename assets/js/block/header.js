99677242a18dd39a44ae7241edf425eb17af36e5
Hirumago <kevin.guillier.pro@gmail.com>
2019-11-06 13:54:10 +0100

    patch header

--- assets/js/block/header.js
+++ assets/js/block/header.js
@@ -1,6 +1,20 @@
-$(function(){
-    let menu = $("header ul li");
-    menu.click(function(){
-       $(this).find('a').click();
-   })
+$(function () {
+    let menu = $("header nav ul li");
+    let menu_images = $("header nav ul li img");
+    let articles = $("article");
+
+    menu.click(function () {
+        $("header ul li.active").removeClass('active');
+        $(this).addClass('active');
+        let article = $(this).attr('data-article');
+        articles.removeClass('active');
+        $("article[data-article='"+article+"']").addClass('active');
+    });
+    menu_images.click(function () {
+        $("header ul li.active").removeClass('active');
+        $(this.parent).addClass('active');
+        let article = $(this.parent).attr('data-article');
+        articles.removeClass('active');
+        $("article[data-article='"+article+"']").addClass('active');
+    })
 });
