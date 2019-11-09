0c4399007759b54f9ca4e16a6c8e860d7c613f55
Hirumago <kevin.guillier.pro@gmail.com>
2019-11-10 00:30:55 +0100

    mise en page en cours

--- includes/page/index.php
+++ includes/page/index.php
@@ -1,6 +1,6 @@
 <section>
     <article data-article="bio" class="active">
-        <h2>Qui suis-je ?</h2>
+        <img src="assets/image/ardoise-1.svg" alt="banner who am i">
         <div class="row" id="who-am-i">
             <div class="col-md-4 hidden-sm hidden-xs" id="bloc-linkedin">
                 <img src="assets/image/photo-2.jpg" alt="photo kevin guillier" class="photo">
@@ -17,102 +17,115 @@
         </div>
     </article>
     <article data-article="cv">
-        <h2>Curriculum Vitae</h2>
-        <div class="row" data-type="cv">
-            <div class="col-xs-12 col-md-4" data-type="poste">
-                <p>Mai 2018 - En cours</p>
-                <p>Concepteur Développeur</p>
-                <p>MBP DIGITAL, Nantes</p>
-            </div>
-            <div class="col-xs-12 col-md-8">
-                <p>Ajout et maintient de fonctionnalités du CRM Leizee. Création de site web et de nouvelles solutions en immobilier.</p>
-            </div>
-
-        </div>
-        <div class="row" data-type="cv">
-            <div class="col-xs-12 col-md-8">
-                <p>Création d’un outil de suivi des stats de club : création de projets, création de feuilles de graphiques, ajout de mesures (indicateur numérique, diagramme secteur, diagramme radar, histogramme) en fonction des données. Script utilisé : d3.js Langages : HTML/PHP/JavaScript.</p>
-            </div>
-            <div class="col-xs-12 col-md-4" data-type="poste">
-                <p>Décembre 2016 - Mars 2017</p>
-                <p>DÉVELOPPEUR</p>
-                <p>ASMOZA, Nantes</p>
-            </div>
-        </div>
-        <div class="row" data-type="cv">
-            <div class="col-xs-12 col-md-4" data-type="poste">
-                <p>Mai 2019 - En cours</p>
-                <p>Chef de projet en conception de SI</p>
-                <p>IMIE / CAMPUS ACADEMY, Nantes</p>
-            </div>
-            <div class="col-xs-12 col-md-8">
-                <p>Les objectifs d'apprentissage de la formation : </p>
-                <ul>
-                    <li>Développer des compétences de veille technologique et d'évolution du SI.</li>
-                    <li>Développer des compétences techniques, managériales (de projet ou d'équipe notamment), organisationnelles et/ou communicationnelles.</li>
-                </ul>
-            </div>
-        </div>
-        <div class="row" data-type="cv">
-            <div class="col-xs-12 col-md-8">
-                <p>Les objectifs d'apprentissage de la formation : </p>
-                <ul>
-                    <li>Développer des composants d'interface</li>
-                    <li>Développer une application n-tiers</li>
-                    <li>Développer la persistance des données</li>
-                </ul>
-            </div>
-
-            <div class="col-xs-12 col-md-4" data-type="formation">
-                <p>Mai 2018 - Mai 2019</p>
-                <p>Concepteur développeur informatique</p>
-                <p>IMIE, Nantes</p>
-            </div>
-        </div>
-
-        <div class="row" data-type="cv">
-            <div class="col-xs-12 col-md-4" data-type="formation">
-                <p>Février 2018 - Mai 2018</p>
-                <p>Développeur web mobile (POEC)</p>
-                <p>IMIE, Nantes</p>
-            </div>
-
-            <div class="col-xs-12 col-md-8">
-                <p>Apprentissage avancé de PHP et JavaScript, Méthodes Agile, SGBD. Re/découverte des frameworks : Symfony, Angular, Ionic. Développement dans un environnement Android.</p>
-            </div>
-        </div>
-        <div class="row" data-type="cv">
-            <div class="col-xs-12 col-md-8">
-                <p>Analyse des besoins, Gestion de projet, Maquettage, HTML/CSS, SQL, PHP, JavaScript, langage Objet, découverte Symfony</p>
-            </div>
-
-            <div class="col-xs-12 col-md-4" data-type="formation">
-                <p>Juin 2016 - Mars 2017</p>
-                <p>Développeur logiciel</p>
-                <p>IMIE, Nantes</p>
-            </div>
-        </div>
+        <img src="assets/image/ardoise-2.svg" alt="banner curriculum vitae">
         <div class="row">
-            <div class="col-sm-12 col-md-8">
+            <div class="col-xs-12 col-md-6 card">
+                <div>
+                    <img src="assets/image/cv-1.svg" alt="">
+                    <div>
+                        <p>Mai 2019 - <span class="c-green">En cours</span> / Concepteur Développeur / MBP DIGITAL, Nantes</p>
+                        <p>Ajout et maintient de fonctionnalités du CRM Leizee. Création de site web et de nouvelles solutions en immobilier.</p>
+                        <p>Participation à l'organisation des différents projet.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 card">
+                <div>
+                    <img src="assets/image/cv-2.svg" alt="">
+                    <div>
+                        <p>Mai 2018 - Mai 2019 / Concepteur Développeur / MBP DIGITAL, Nantes</p>
+                        <p>Ajout et maintient de fonctionnalités du CRM Leizee. Création de site web et de nouvelles solutions en immobilier.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-md-6 card col-md-offset-3">
+                <div>
+                    <img src="assets/image/cv-3.svg" alt="">
+                    <div>
+                        <p>Décembre 2016 - Mars 2017 / Développeur / ASMOZA, Nantes</p>
+                        <p>Création d’un outil de suivi des stats de club : création de projets, création de feuilles de graphiques, ajout de mesures (indicateur numérique, diagramme secteur, diagramme radar, histogramme) en fonction des données. Script utilisé : d3.js Langages : HTML/PHP/JavaScript.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-md-6 card">
+                <div>
+                    <img src="assets/image/cv-4.svg" alt="">
+                    <div>
+                        <p>Mai 2019 - <span class="c-green">En cours</span> / Chef de projet en conception de SI / IMIE / CAMPUS ACADEMY, Nantes</p>
+                        <p>Les objectifs d'apprentissage de la formation : </p>
+                        <ul>
+                            <li>Développer des compétences de veille technologique et d'évolution du SI.</li>
+                            <li>Développer des compétences techniques, managériales (de projet ou d'équipe notamment), organisationnelles et/ou communicationnelles.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 card">
+                <div>
+                    <img src="assets/image/cv-5.svg" alt="">
+                    <div>
+                        <p>Mai 2018 - Mai 2019 / Concepteur développeur informatique / IMIE, Nantes</p>
+                        <p>Les objectifs d'apprentissage de la formation : </p>
+                        <ul>
+                            <li>Développer des composants d'interface</li>
+                            <li>Développer une application n-tiers</li>
+                            <li>Développer la persistance des données</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-md-6 card">
+                <div>
+                    <img src="assets/image/cv-6.svg" alt="">
+                    <div>
+                        <p>Février 2018 - Mai 2018 / Développeur web mobile (POEC) / IMIE, Nantes</p>
+                        <p>Apprentissage avancé de PHP et JavaScript, Méthodes Agile, SGBD. Re/découverte des frameworks : Symfony, Angular, Ionic. Développement dans un environnement Android.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-md-6 card">
+                <div>
+                    <img src="assets/image/cv-7.svg" alt="">
+                    <div>
+                        <p>Juin 2016 - Mars 2017 / Développeur logiciel / IMIE, Nantes</p>
+                        <p>Analyse des besoins, Gestion de projet, Maquettage, HTML/CSS, SQL, PHP, JavaScript, langage Objet, découverte Symfony</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="row">
+            <div class="col-xs-12">
                 <div class="row">
-                    <div class="col-sm-12 col-md-3 ta-center">
+                    <div class="col-xs-12 col-md-3 ta-center">
                         <p>Front-end</p>
                         <div id="g1" class="gauge" data-label="HTML/CSS" data-value="9"></div>
                         <div id="g2" class="gauge" data-label="Javascript" data-value="7"></div>
                         <div id="g3" class="gauge" data-label="React" data-value="1"></div>
                     </div>
-                    <div class="col-sm-12 col-md-3 ta-center">
+                    <div class="col-xs-12 col-md-3 ta-center">
                         <p>Back-end</p>
                         <div id="g4" class="gauge" data-label="SQL/BDD" data-value="7"></div>
                         <div id="g5" class="gauge" data-label="PHP" data-value="7"></div>
                     </div>
-                    <div class="col-sm-12 col-md-3 ta-center">
+                    <div class="col-xs-12 col-md-3 ta-center">
                         <p>Framework</p>
                         <div id="g6" class="gauge" data-label="Symfony" data-value="2"></div>
                         <div id="g7" class="gauge" data-label="Ionic" data-value="5"></div>
                     </div>
 
-                    <div class="col-sm-12 col-md-3 ta-center">
+                    <div class="col-xs-12 col-md-3 ta-center">
                         <p>Autres</p>
                         <div id="g8" class="gauge" data-label="Maquettage" data-value="7"></div>
                         <div id="g9" class="gauge" data-label="Gestion de projet" data-value="4"></div>
@@ -122,7 +135,7 @@
         </div>
     </article>
     <article data-article="twitch">
-        <h2>Streaming</h2>
+        <img src="assets/image/ardoise-3.svg" alt="banner streaming">
         <div class="row">
             <div class="col-xs-12 col-md-6">
                 <iframe
