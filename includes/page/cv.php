e1c2537f92724f19d9362068fafa5c482b56fe11
Hirumago <kevin.guillier.pro@gmail.com>
2020-01-20 22:10:46 +0100

    Ajustement dates cv

--- includes/page/cv.php
+++ includes/page/cv.php
@@ -1,16 +1,16 @@
 <section>
     <div>
         <div id="cv" class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            <div class="col-xs-5 cv-title">
+            <div class="col-xs-5 col-md-4 cv-title">
                 Expériences
             </div>
-            <div class="col-xs-2 cv-title">
+            <div class="col-xs-2 col-md-4 cv-title">
                 Date
             </div>
-            <div class="col-xs-5 cv-title">
+            <div class="col-xs-5 col-md-4 cv-title">
                 Formation
             </div>
-            <div class="col-xs-5 cv-card">
+            <div class="col-xs-5 col-md-4 cv-card">
                 <div>
                     <p>Concepteur Développeur</p>
                     <p>MBP DIGITAL, Nantes</p>
@@ -25,25 +25,33 @@
                     <p>ASMOZA, Nantes</p>
                 </div>
             </div>
-            <div class="col-xs-2 cv-tree">
+            <div class="col-xs-2 col-md-4 cv-tree">
+                <div class="cv-tree-desktop">Mai 2019</div>
+                <div class="cv-tree-desktop c-green">En cours</div>
                 <div class="cv-tree-round">
                     <p>Mai 2019 - <span class="c-green">En cours</span></p>
                 </div>
                 <div class="cv-tree-bar"></div>
+                <div class="cv-tree-desktop">Mai 2018</div>
+                <div class="cv-tree-desktop c-green">Mai 2019</div>
                 <div class="cv-tree-round">
                     <p>Mai 2018 - Mai 2019</p>
                 </div>
                 <div class="cv-tree-bar"></div>
+                <div class="cv-tree-desktop">Décembre 2016</div>
+                <div class="cv-tree-desktop c-green">Mars 2017</div>
                 <div class="cv-tree-round">
                     <p>Décembre 2016 - Mars 2017</p>
                 </div>
                 <div class="cv-tree-bar"></div>
+                <div class="cv-tree-desktop">Mars 2016</div>
+                <div class="cv-tree-desktop c-green">Mars 2017</div>
                 <div class="cv-tree-round">
                     <p>Mars 2016 - Mars 2017</p>
                 </div>
                 <div class="cv-tree-bar"></div>
             </div>
-            <div class="col-xs-5 cv-card">
+            <div class="col-xs-5 col-md-4 cv-card">
                 <div>
                     <p>Chef de projet en conception de SI</p>
                     <p>IMIE - Campus Academy, Nantes</p>
